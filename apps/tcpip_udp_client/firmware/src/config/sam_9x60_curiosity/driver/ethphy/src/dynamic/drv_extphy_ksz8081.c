dfd4e3cc52bffb00ed312c7e653f00418dc8a654
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-04-01 10:29:41 +0530

    [MH3-83693] - tcpip_udp_client application regeneration - SAM9X60

--- apps/tcpip_udp_client/firmware/src/config/sam_9x60_curiosity/driver/ethphy/src/dynamic/drv_extphy_ksz8081.c
+++ apps/tcpip_udp_client/firmware/src/config/sam_9x60_curiosity/driver/ethphy/src/dynamic/drv_extphy_ksz8081.c
@@ -212,7 +212,7 @@ static DRV_ETHPHY_RESULT DRV_EXTPHY_MDIXConfigure(const DRV_ETHPHY_OBJECT_BASE* 
  *****************************************************************************/
 static unsigned int DRV_EXTPHY_SMIClockGet(const DRV_ETHPHY_OBJECT_BASE* pBaseObj, DRV_HANDLE handle)
 {
-    return 2500000;     //  2.5 MHz max clock supported
+    return 10000000;        //  10 MHz max clock supported
 }
 
 // the DRV_ETHPHY_OBJECT
@@ -224,5 +224,7 @@ const DRV_ETHPHY_OBJECT  DRV_ETHPHY_OBJECT_KSZ8081 =
     .smiClockGet = DRV_EXTPHY_SMIClockGet,
     .wolConfigure = 0,                      // no WOL functionality yet
     .phyDetect = 0,                         // default detection performed
+    .bmconDetectMask = 0,                   // standard detection mask
+    .bmstatCpblMask = 0,                    // standard capabilities mask
 };
 
